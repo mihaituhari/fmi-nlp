36c9f311cb00ed162d0d9ec7da99a624b2b16a8a
lorenapotlog <lorena.potlog@yahoo.com>
2025-04-14 17:19:21 +0300

    add sanitization

--- clean_data.py
+++ clean_data.py
@@ -19,18 +19,23 @@ label_map = {0: "pessimist", 1: "neutral", 2: "optimist"}
 df["label_name"] = df["label"].map(label_map)
 
 def clean_text(text):
+    text = html.unescape(text)
     # Elimină @user
     text = re.sub(r'@\w+', '', text)
     # Elimină URL-uri
     text = re.sub(r'http\S+', '', text)
+    # Elimină emoji-urile
+    text = re.sub(r'[^\w\s' + re.escape(string.punctuation) + ']', '', text)
     # Elimină caracterele non-ASCII
     text = re.sub(r'[^\x00-\x7F]+', '', text)
+    # Elimină semnele de punctuație repetitive excesive, înafara de ?!
+    text = re.sub(r'[^\w\s!?]', '', text)
     # Elimină semnele de punctuație repetitive"
-    text = re.sub(r'[!?.]{2,}', '!', text)
+    text = re.sub(r'([!?]){2,}', r'\1', text)
     text = text.lower()
     # Înlocuiește spațiile multiple cu un singur spațiu
     text = re.sub(r'\s+', ' ', text)
-    text = text.replace('gr8', 'great')
+    text = text.replace('gr8', 'great').replace('b4', 'before').replace('luv', 'love')
     return text
 
 df["text"] = df["text"].apply(clean_text)
